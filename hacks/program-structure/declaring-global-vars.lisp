195751c5cdb87ea2656610f1a4467afbda8af108
Toby M <hk.mars@aol.com>
2019-01-31 14:46:42 +0800

    Update declaring-global-vars.lisp

--- hacks/program-structure/declaring-global-vars.lisp
+++ hacks/program-structure/declaring-global-vars.lisp
@@ -6,10 +6,18 @@
 * (defvar *compiler-switch-number* 10)
 
 *COMPILER-SWITCH-NUMBER*
+
+;;
+;; correct, because the call to **format** is evaluated at read time
+;; (only at load time, or when interpreted but not compiled).
+;;
 * (defvar *avoid-registers* nil 
   #.(format nil "Compilation control switch #~D" 
             (incf *compiler-switch-number*)))
 
+;;
+;; erroneous because the all to format is not a literal string.
+;;
 *AVOID-REGISTERS*
 * (defvar *avoid-registers* nil 
   (format nil "Compilation control switch #~D" 
