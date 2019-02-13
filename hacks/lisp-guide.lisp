a3842ebb657b4e2dd71c4ca8eadab1b141df6dad
TobyM <hk.mars@aol.com>
2019-02-13 23:56:55 +0800

    Improved.

--- hacks/lisp-guide.lisp
+++ hacks/lisp-guide.lisp
@@ -2,7 +2,7 @@
 ;;
 ;; a guide for those who want to be the best lisp hacker.
 ;; 
-;; 1. Do practices as more as possible to have the systemic understanding of common lisp.
+;; 1. Do enough practices here to have a systemic understanding of common lisp.
 ;; 2. Try to find new patterns of language design via those practices.
 ;;
 ;;
