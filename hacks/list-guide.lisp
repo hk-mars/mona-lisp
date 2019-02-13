6aae586aae2e4512ac2ac255d105e02cd5be0942
Toby M <hk.mars@aol.com>
2019-02-13 23:55:21 +0800

    Delete list-guide.lisp

--- hacks/list-guide.lisp
+++
@@ -1,8 +0,0 @@
-
-;;
-;; a guide for those who want to be the best lisp hacker.
-;; 
-;; 1. Do practices as more as possible to have the systemic understanding of common lisp.
-;; 2. Try to find new patterns of language design via those practices.
-;;
-;;
