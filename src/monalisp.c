f1e0713df9108d3ffcd56fc17067ddf8ab18a600
TobyM <hk.mars@aol.com>
2019-06-01 15:13:32 +0800

    Add: files of syntax, project-build, parser entry.

---
+++ src/monalisp.c
@@ -0,0 +1,14 @@
+
+#define MONA_OK
+#define VER v1.0.0
+
+
+int 
+main (int argc, char **argv)
+{
+    printf("hello monalisp %s", VER);
+
+    return MONA_OK;
+}
+
+
---
+++ syntax.md
@@ -0,0 +1,11 @@
+
+
+- Monalisp Syntax
+
+Syntax with extended BNF forms:
+
+{A} means 0 or more as,
+
+[A] means an optional A,
+
+
