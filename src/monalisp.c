7f02f3e1b1c3468f3b3adfbd6ba55d9b2f9bccd4
TobyM <hk.mars@aol.com>
2019-06-02 14:03:40 +0800

    1. Create the build makefile. 2. Show "hello monalisp" and version in monalisp.c.

--- src/monalisp.c
+++ src/monalisp.c
@@ -1,12 +1,18 @@
 
-#define MONA_OK
+#include <stdio.h>
+
+
+#define MONA_OK 0
+
+
 #define VER "v1.0.0"
 
 
 int 
 main (int argc, char **argv)
 {
-    printf("hello monalisp %s", VER);
+    printf("hello monalisp \n");
+    printf("version: %s \n", VER);
 
     return MONA_OK;
 }
